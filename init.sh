2a70f032b49623a4d7663436c4560e9d06a173c9
daniele <giglio.d@gmail.com>
2015-02-25 13:35:26 +0100

    fixed some errors

--- init.sh
+++ init.sh
@@ -2,9 +2,5 @@
 
 IP=$(ip -o -4 addr list eth0 | awk '{print $4}' | cut -f1 -d'/');
 
-sed -ir "s/([0-9]{1,3}\.){3}[0-9]{1,3}/$IP/g" /etc/kamailio/kamctlrc;
-sed -ir "s/([0-9]{1,3}\.){3}[0-9]{1,3}/$IP/g" /etc/kamailio/kamailio.cfg;
-
-service rtpproxy start;
-
-service kamailio start;
+sed -ri "s/([0-9]{1,3}\.){3}[0-9]{1,3}/$IP/" /etc/kamailio/kamctlrc;
+sed -ri "s/(MY_IP_ADDR\!)([0-9]{1,3}\.){3}[0-9]{1,3}/\1$IP/" /etc/kamailio/kamailio.cfg;
