83f1741a161d4b8ea531fd7a2393e40f794da245
daniele <giglio.d@gmail.com>
2015-02-26 18:53:01 +0100

    $DOMAIN via enviromental varable

--- init.sh
+++ init.sh
@@ -4,3 +4,9 @@ IP=$(ip -o -4 addr list eth0 | awk '{print $4}' | cut -f1 -d'/');
 
 sed -ri "s/([0-9]{1,3}\.){3}[0-9]{1,3}/$IP/" /etc/kamailio/kamctlrc;
 sed -ri "s/(MY_IP_ADDR\!)([0-9]{1,3}\.){3}[0-9]{1,3}/\1$IP/" /etc/kamailio/kamailio.cfg;
+
+sed -ri "s/SIP_DOMAIN=.*/SIP_DOMAIN=$DOMAIN/" /etc/kamailio/kamctlrc;
+sed -ri "s/DOMAIN_ALIAS=.*/DOMAIN_ALIAS=$DOMAIN/" /etc/kamailio/kamctlrc;
+sed -ri "s/MY_DOMAIN=.*/MY_DOMAIN=$DOMAIN/" /etc/kamailio/kamctlrc;
+sed -ri "s/DBACCESSHOST=.*/DBACCESSHOST=$DOMAIN/" /etc/kamailio/kamctlrc;
+
