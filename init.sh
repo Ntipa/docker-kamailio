ea174af55f19f47d1f6c59b084a4feaf632a586b
daniele <giglio.d@gmail.com>
2015-02-26 19:11:55 +0100

    - make kamailio listen on all address - disable relayng check

--- init.sh
+++ init.sh
@@ -2,11 +2,12 @@
 
 IP=$(ip -o -4 addr list eth0 | awk '{print $4}' | cut -f1 -d'/');
 
-sed -ri "s/([0-9]{1,3}\.){3}[0-9]{1,3}/$IP/" /etc/kamailio/kamctlrc;
+sed -ri "s/([0-9]{1,3}\.){3}[0-9]{1,3}/0.0.0.0/" /etc/kamailio/kamctlrc;
 sed -ri "s/(MY_IP_ADDR\!)([0-9]{1,3}\.){3}[0-9]{1,3}/\1$IP/" /etc/kamailio/kamailio.cfg;
 
-sed -ri "s/SIP_DOMAIN=.*/SIP_DOMAIN=$DOMAIN/" /etc/kamailio/kamctlrc;
-sed -ri "s/DOMAIN_ALIAS=.*/DOMAIN_ALIAS=$DOMAIN/" /etc/kamailio/kamctlrc;
-sed -ri "s/MY_DOMAIN=.*/MY_DOMAIN=$DOMAIN/" /etc/kamailio/kamctlrc;
-sed -ri "s/DBACCESSHOST=.*/DBACCESSHOST=$DOMAIN/" /etc/kamailio/kamctlrc;
-
+if [ "$CHARSET" != "" ] ; then
+    sed -ri "s/SIP_DOMAIN=.*/SIP_DOMAIN=$DOMAIN/" /etc/kamailio/kamctlrc;
+    sed -ri "s/DOMAIN_ALIAS=.*/DOMAIN_ALIAS=$DOMAIN/" /etc/kamailio/kamctlrc;
+    sed -ri "s/MY_DOMAIN=.*/MY_DOMAIN=$DOMAIN/" /etc/kamailio/kamctlrc;
+    sed -ri "s/DBACCESSHOST=.*/DBACCESSHOST=$DOMAIN/" /etc/kamailio/kamctlrc;
+fi
