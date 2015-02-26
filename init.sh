d8ed8e605411a77eb205c023c34ef4e2eee43964
daniele <giglio.d@gmail.com>
2015-02-26 19:32:37 +0100

    enable debug mode

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 IP=$(ip -o -4 addr list eth0 | awk '{print $4}' | cut -f1 -d'/');
-IP=0.0.0.0
+#IP=0.0.0.0
 
 sed -ri "s/([0-9]{1,3}\.){3}[0-9]{1,3}/$IP/" /etc/kamailio/kamctlrc;
 sed -ri "s/(MY_IP_ADDR\!)([0-9]{1,3}\.){3}[0-9]{1,3}/\1$IP/" /etc/kamailio/kamailio.cfg;
