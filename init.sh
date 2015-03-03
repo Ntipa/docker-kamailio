cd9a0e7d8d5d740dde52a0f05a51c19eb3c38f8e
daniele <giglio.d@gmail.com>
2015-03-03 18:34:47 +0100

    useless variables removed

--- init.sh
+++ init.sh
@@ -6,10 +6,10 @@ sed -ri "s/([0-9]{1,3}\.){3}[0-9]{1,3}/$IP/" /etc/kamailio/kamctlrc;
 sed -ri "s/(MY_IP_ADDR\!)([0-9]{1,3}\.){3}[0-9]{1,3}/\1$IP/" /etc/kamailio/kamailio.cfg;
 sed -ri "s/PUBLIC_IP=.*/PUBLIC_IP=$PUBLIC_IP/" /etc/kamailio/kamctlrc;
 sed -ri "s/PUBLIC_IP/$PUBLIC_IP/g" /etc/kamailio/kamailio.cfg;
-sed -ri "s/SIP_DOMAIN/$DOMAIN/g" /etc/kamailio/kamailio.cfg;
-sed -ri "s/PUBLIC_IP/$PUBLIC_IP/g" /etc/default/rtpproxy;
+sed -ri "s/PUBLIC_IP/$PUBLIC_IP/" /etc/default/rtpproxy;
 
 if [ "$DOMAIN" != "" ] ; then
+    sed -ri "s/SIP_DOMAIN/$DOMAIN/g" /etc/kamailio/kamailio.cfg;
     sed -ri "s/SIP_DOMAIN=.*/SIP_DOMAIN=$DOMAIN/" /etc/kamailio/kamctlrc;
     sed -ri "s/SIP_DOMAIN=/$DOMAIN/g" /etc/kamailio/kamailio.cfg;
     sed -ri "s/DOMAIN_ALIAS=.*/DOMAIN_ALIAS=$DOMAIN/" /etc/kamailio/kamctlrc;
