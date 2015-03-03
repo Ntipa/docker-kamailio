7fc4375547b6f25cfa30ddc9330d002d35a6fe29
daniele <giglio.d@gmail.com>
2015-03-03 12:43:55 +0100

    Changes to be committed: 	modified:   kamailio.cfg    (removed useless variable) 	modified:   rtpproxy.sh	    (path corrected) 	modified:   supervisord.conf

--- rtpproxy.sh
+++ rtpproxy.sh
@@ -2,4 +2,4 @@
 
 PRIV_IP=$(ip -o -4 addr list eth0 | awk '{print $4}' | cut -f1 -d'/')
 
-/usr/bin/rtpproxy -s udp:127.0.0.1:7722 -u kamailio:kamailio -p /var/run/rtpproxy/rtpproxy.pid -d WARN:LOG_LOCAL5 -l $PRIV_IP/$PUBLIC_IP -m 10001 -M 10010
+/usr/sbin/rtpproxy -s udp:127.0.0.1:7722 -u kamailio:kamailio -p /var/run/rtpproxy/rtpproxy.pid -d WARN:LOG_LOCAL5 -l $PRIV_IP/$PUBLIC_IP -m 10001 -M 10010
